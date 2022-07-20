e175d303a128a2b0b05006c86bbca71229e3b0fc
jhontovar123 <jhon.tovar@utec.edu.pe>
2022-07-20 18:56:05 -0500

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -201,7 +201,7 @@ if resultado==6:
 st.subheader('XGBoost Model Predictions')
 st.write('$\\phi$ = Resistance Factor Design')  
 w_cr_results={'Shear Strength (kN)':"{:.2f}".format(V_test),
-               '$\phi$' 'Shear Strength (kN)':"{:.2f}".format(V_test*phi),
+               '𝜙Shear Strength (kN)':"{:.2f}".format(V_test*phi),
                'Failure Mode':format(res)}
 
 w_cr_results_df=pd.DataFrame(w_cr_results, index=[0])
